49a4d984cb1c29a55f7388921e42934f81842c02
hashimoto-hayato <e1n22054@oit.ac.jp>
2025-07-06 16:59:40 +0900

    Add tests for Calculate methosd

--- app/src/test/java/org/example/CalculateTest.java
+++ app/src/test/java/org/example/CalculateTest.java
@@ -4,11 +4,34 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CalculateTest {
-    @Test
-    public void testSum() {
-        Calculate calculate = new Calculate();
-        int expected = 5;
-        assertEquals(expected, calculate.sum(2, 3));
-    }
-    
+
+  @Test
+  public void testSum() {
+    Calculate calc = new Calculate();
+    assertEquals(5, calc.sum(2, 3));
+  }
+
+  @Test
+  public void testAverage() {
+    Calculate calc = new Calculate();
+    assertEquals(2.5, calc.average(2, 3), 0.001); // 第3引数は誤差許容範囲
+  }
+
+  @Test
+  public void testSumBetween() {
+    Calculate calc = new Calculate();
+    assertEquals(55, calc.sumBetween(1, 10)); // 1+2+...+10 = 55
+  }
+
+  @Test
+  public void testSumOdd() {
+    Calculate calc = new Calculate();
+    assertEquals(25, calc.sumOdd(1, 10)); // 1+3+5+7+9
+  }
+
+  @Test
+  public void testSumEven() {
+    Calculate calc = new Calculate();
+    assertEquals(30, calc.sumEven(1, 10)); // 2+4+6+8+10
+  }
 }
